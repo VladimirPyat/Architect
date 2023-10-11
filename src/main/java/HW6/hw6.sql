c8b39c501535d6bd6188f1e56b67c43e7344b165
Vladimir Pyat <scared@mail.ru>
2023-10-11 17:08:19 +0300

    HW6

---
+++ src/main/java/HW6/hw6.sql
@@ -0,0 +1,18 @@
+ALTER TABLE `Doc` DROP FOREIGN KEY `Doc_fk0`;
+
+ALTER TABLE `Doc` DROP FOREIGN KEY `Doc_fk1`;
+
+ALTER TABLE `Shedule` DROP FOREIGN KEY `Shedule_fk0`;
+
+ALTER TABLE `Shedule` DROP FOREIGN KEY `Shedule_fk1`;
+
+ALTER TABLE `Shedule` DROP FOREIGN KEY `Shedule_fk2`;
+
+ALTER TABLE `Diagnos` DROP FOREIGN KEY `Diagnos_fk0`;
+
+ALTER TABLE `Diagnos` DROP FOREIGN KEY `Diagnos_fk1`;
+
+ALTER TABLE `History` DROP FOREIGN KEY `History_fk0`;
+
+ALTER TABLE `History` DROP FOREIGN KEY `History_fk1`;
+
